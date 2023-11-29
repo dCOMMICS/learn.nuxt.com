01000b710f0642ec01a97e2c63fd31358b10453e
dCOMMICS <sanwurld@gmail.com>
2023-11-29 20:44:05 +0300

    great 😪

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+
+<script setup lang="ts">
+
+const props = defineProps<{
+    moduleValue?: boolean,
+
+}>()
+</script>
+
+<template>
+    <div>
+        Hello Muthored
+        <slot/>
+    </div>
+</template>
